1deb330cdc99d036d31dac2c3d438944f8bf04b5
MinJu Kim <rorrxr@naver.com>
2024-11-14 16:47:38 +0900

    [Bronze V] Title: 이상한 기호, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.py
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.py
@@ -1,10 +1,4 @@
 # A@B = (A+B)×(A-B)
 
-def cal(a, b):
-    if(a >= 1000 or b >= 1000):
-      return
-    else:
-      return (a+b)*(a-b)
-
 a, b = map(int, input().split())
-print(cal(a, b))
+print((a+b)*(a-b))
