efa3913f5586f83794d3840023e2d1dc25f9ce17
Dev_chu <rorrxr@naver.com>
2024-11-30 19:15:34 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 132 ms, Memory: 37176 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
@@ -1,38 +1,29 @@
 import sys, heapq
 input = sys.stdin.readline
 
-# 입력 받기
+# 1. 각 데이터 입력받기
 n, h, t = map(int, input().split())
 
-# 거인들의 키를 음수로 변환하여 리스트에 저장 (최대 힙처럼 사용)
+# 2. 거인들의 키 heapq 리스트로 저장
 giants = [-int(input()) for _ in range(n)]
-
-# giants = []
-# for _ in range(n):
-#     heapq.heappush(giants, -int(input()))
-
-# 힙으로 변환
 heapq.heapify(giants)
-
+# 망치질 횟수 저장
 cnt = 0
 
-# 마법을 T번까지 사용하거나 거인의 키가 센티보다 작아지면 종료
+# 3. 망치질 횟수만큼 반복
 for i in range(t):
-    
-    # 현재 가장 큰 거인의 키가 1이거나 센티보다 작은 경우 마법을 멈춤
+    # 1) 가장 큰 거인이 1명이거나, 센티보다 키가 작을 때 멈춤
     if -giants[0] == 1 or -giants[0] < h:
         break
-    
     else:
-        # 가장 큰 거인의 키를 절반으로 줄이고 다시 힙에 넣기
-        heapq.heapreplace(giants, -(-giants[0] // 2))  # 절반으로 줄이기
+        # 그 외에는 peapq에서 제일 큰 값을 /2를 하여 giants에 저장
+        heapq.heapreplace(giants, -(-giants[0] // 2))
+        # 망치질 횟수 1 증가
         cnt += 1
 
-# 마법을 사용하고 나서, 가장 큰 거인의 키가 센티보다 크다면 NO 출력
+# 만약 가장 큰 키가 센티보다 크다면?
 if -giants[0] >= h:
-    print('NO')
-    print(-giants[0])
+    print('NO', -giants[0], sep='\n')
+    # 그 외는?
 else:
-    # 모든 거인의 키가 센티보다 작아지면 YES와 마법 사용 횟수 출력
-    print('YES')
-    print(cnt)
+    print('YES', cnt, sep='\n')
