509e27387350a490b9382c37aed5b0bd107d6d55
MinJu Kim <rorrxr@naver.com>
2025-08-04 12:35:18 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -0,0 +1,14 @@
+SELECT 
+    E.ID,
+    COUNT(C.ID) AS CHILD_COUNT
+FROM 
+-- 부모
+    ECOLI_DATA E
+-- 자식
+LEFT JOIN 
+    ECOLI_DATA C
+    ON E.ID = C.PARENT_ID
+GROUP BY 
+    E.ID
+ORDER BY 
+    E.ID
